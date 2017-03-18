ec648940d32af3fe539ceb4e1b75cf30377fa377
zzy <625291711@qq.com>
2017-03-18 21:45:20 +0000

    优化博客编辑窗口

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from DjangoUeditor.models import UEditorField
 
 # Create your models here.
 
@@ -23,7 +24,9 @@ class Blog(models.Model):
     """博客信息"""
     title = models.CharField(verbose_name="标题", max_length=50)
     id = models.AutoField(verbose_name="id", primary_key=True)
-    text = models.TextField(verbose_name="内容", blank=True)
+    text = UEditorField("内容", height=300, width=1000,
+                        default='在此输入', blank=True, imagePath="uploads/images/",
+                        toolbars='besttome', filePath='uploads/files/')
     comment = models.TextField(verbose_name="评论", blank=True, null=True)
     clicks = models.IntegerField(verbose_name="点击量", default=0)
     datetime = models.DateTimeField(verbose_name="提交时间", auto_now=True)
